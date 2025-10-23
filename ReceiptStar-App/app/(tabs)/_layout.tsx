246887af410d5a56a6e8ef165293e134320e1c74
ktwnly <ktnoles@1791.com>
2025-10-23 14:04:51 -0400

    added tab icons
    Replaced tab icons for all tabs, changed color and size, and rearranged their positions.

--- ReceiptStar-App/app/(tabs)/_layout.tsx
+++ ReceiptStar-App/app/(tabs)/_layout.tsx
@@ -2,9 +2,11 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
+
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,6 +14,7 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        tabBarShowLabel: false,
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
@@ -19,17 +22,45 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={30} name="home" color="#cccbff" />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={30} name="search" color="#cccbff" />,
         }}
       />
+
+      <Tabs.Screen
+        name="camera"
+        options={{
+        tabBarIcon: ({ color }) => <Ionicons size={30} name="camera" color="#cccbff" />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="data"
+        options={{
+        tabBarIcon: ({ color }) => <Ionicons size={30} name="menu-outline" color="#cccbff" />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+        tabBarIcon: ({ color }) => <Ionicons size={30} name="person" color="#cccbff" />,
+        }}
+      />
+
+      
+
+      
+
+
+
+
+
     </Tabs>
   );
 }
