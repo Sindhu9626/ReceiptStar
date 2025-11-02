bbfab819fa87a93af40846edf781d95fdfe3ade6
mikaylaphilpot <mikaylagp@hotmail.com>
2025-11-02 13:03:35 -0500

    add login screen to navigation stack, remove redundant code

--- ReceiptStar-App/app/_layout.tsx
+++ ReceiptStar-App/app/_layout.tsx
@@ -1,8 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import {checkCurrentUser} from '../src/checkLogin';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
@@ -12,19 +9,14 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  let name;
-  if(checkCurrentUser != null) {
-    name = "(tabs)"
-  }
-  else {
-    name = "loginScreen"
-  }
   return (
     <Stack screenOptions={{ headerShown: false, animation: "fade", }}>
       {/* first screen (redirected to from app/index) */}
       <Stack.Screen name="loadingWelcome" />
       {/* your tabs group */}
-      <Stack.Screen name={name} />
+      <Stack.Screen name="(tabs)" />
+      {/*login */}
+      <Stack.Screen name="loginScreen" />
     </Stack>
     
   );
