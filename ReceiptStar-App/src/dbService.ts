8a228be025cfa43c5b6423ca6a0fe6c574bc617b
mikaylaphilpot <mikaylagp@hotmail.com>
2025-11-13 12:10:22 -0500

    link database
    add comments to create user doc function

--- ReceiptStar-App/src/dbService.ts
+++ ReceiptStar-App/src/dbService.ts
@@ -1,6 +1,7 @@
 import { doc, setDoc } from "firebase/firestore";
 import { db } from './firebaseConfig';
 
+// user doc used to store profile info and link receipts collection to
 export async function createUserDoc(newUserID:string, newUserEmail:string) {
     const newUserDoc = doc(db, "users", newUserID);
 
